87b91637eed2e5011e1438262f7de9a4a59f51a7
demonno <demur.nodia@gmail.com>
2019-07-14 21:20:12 +0300

    Update bots

--- ertaoz_bot.py
+++ ertaoz_bot.py
@@ -1,18 +1,115 @@
-# coding: utf-8
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+#
+# THIS EXAMPLE HAS BEEN UPDATED TO WORK WITH THE BETA VERSION 12 OF PYTHON-TELEGRAM-BOT.
+# If you're still using version 11.1.0, please see the examples at
+# https://github.com/python-telegram-bot/python-telegram-bot/tree/v11.1.0/examples
+
+"""
+Simple Bot to send timed Telegram messages.
+This Bot uses the Updater class to handle the bot and the JobQueue to send
+timed messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Alarm Bot example, sends a message after a set time.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
 import logging
+
+from telegram.ext import Updater, CommandHandler
+
+# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-updater = Updater('726693597:AAGuNw5J2QiDc-C7DKr2Sa4gaQFJy51E4Bc')
-dispatcher = updater.dispatcher
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    update.message.reply_text('ერთაოზ ბრეგვაძე ძუკნურიდან!')
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="ერთაოზ ბრეგვაძე ძუკნურიდან!")
 
-dispatcher.add_handler(CommandHandler('start', start))
+def alarm(context):
+    """Send the alarm message."""
+    job = context.job
+    context.bot.send_message(job.context, text='Beep!')
 
-updater.start_polling()
-updater.idle()
+
+def set_timer(update, context):
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
+
+        # Add job to queue
+        job = context.job_queue.run_once(alarm, due, context=chat_id)
+        context.chat_data['job'] = job
+
+        update.message.reply_text('Timer successfully set!')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
+
+
+def unset(update, context):
+    """Remove the job if the user changed their mind."""
+    if 'job' not in context.chat_data:
+        update.message.reply_text('You have no active timer')
+        return
+
+    job = context.chat_data['job']
+    job.schedule_removal()
+    del context.chat_data['job']
+
+    update.message.reply_text('Timer successfully unset!')
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Run bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("726693597:AAGuNw5J2QiDc-C7DKr2Sa4gaQFJy51E4Bc", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", start))
+    dp.add_handler(CommandHandler("set", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
--- noshrevan_bot.py
+++ noshrevan_bot.py
@@ -1,26 +1,83 @@
-# coding: utf-8
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+#
+# THIS EXAMPLE HAS BEEN UPDATED TO WORK WITH THE BETA VERSION 12 OF PYTHON-TELEGRAM-BOT.
+# If you're still using version 11.1.0, please see the examples at
+# https://github.com/python-telegram-bot/python-telegram-bot/tree/v11.1.0/examples
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
 import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-updater = Updater('684288261:AAHtV3VYrhrcRKOKPzv0HZrCt4hfB41dBxU')
-dispatcher = updater.dispatcher
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('გამარჯობა! მე ვარ doc ნოშრევანი.')
 
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="მე ვარ ნოშრევანი, ვაკეტებ ბროწეულის ოყნებს!")
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('აბე გლუკოზააა!')
 
-def echo(bot, update):
-    print("Incoming: {}".format(update.message.text))
-    bot.send_message(chat_id=update.message.chat_id, text="აბე გლოკოზააა!")
-    
-echo_handler = MessageHandler(Filters.text, echo)
 
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(echo_handler)
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
-updater.start_polling()
-updater.idle()
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("684288261:AAHtV3VYrhrcRKOKPzv0HZrCt4hfB41dBxU", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
