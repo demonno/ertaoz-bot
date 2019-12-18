1fb48e9a63f051326e8888d14337dcf962b2ac25
MadViper <mkenc12@freeuni.edu.ge>
2019-12-19 00:32:29 +0200

    Add users data access layer (#81)

--- dal/__init__.py
+++ dal/__init__.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 from dal.places_dal import PlacesDal
+from dal.users_dal import UsersDal
 from dal.wisdom_dal import WisdomDal
 
 
@@ -8,3 +9,4 @@ from dal.wisdom_dal import WisdomDal
 class DataAccessLayer:
     places: PlacesDal = field(default_factory=PlacesDal)
     wisdoms: WisdomDal = field(default_factory=WisdomDal)
+    users: UsersDal = field(default_factory=UsersDal)
--- dal/models.py
+++ dal/models.py
@@ -2,6 +2,22 @@ from dataclasses import dataclass
 
 
 @dataclass
+class User:
+    id: int
+    first_name: str
+    last_name: str
+    telegram_id: int = 0
+
+    @property
+    def name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def telegram_url(self) -> str:
+        return f"tg://user?id={self.telegram_id}"
+
+
+@dataclass
 class Wisdom:
     text: str
     animation: str
